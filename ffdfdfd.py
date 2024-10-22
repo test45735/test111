5e85a9aa14e6dae598a1eeec04a04689befb1ff5
tttt <dimongamer457@gmail.com>
2024-10-22 19:29:21 +0300

    21.10.20242

--- ffdfdfd.py
+++ ffdfdfd.py
@@ -1,2 +1,21 @@
-print("hello python")
-print("ssdsds")
+print("------------------")
+print("Enter value")
+print("------------------")
+int1 = int(input())
+int2 = int(input())
+
+print("------------------")
+print("Enter which process: plus, minus, multi, division")
+print("------------------")
+
+pr = input()
+if pr == "plus":
+    print(int1 + int2)
+if pr == "minus":
+    print(int1 - int2)
+if pr == "multi":
+    print(int1 * int2)
+if pr == "division":
+    print(int1 / int2)
+
+print("------------------")
