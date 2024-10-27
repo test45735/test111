f0e8780bf3a21f5be4235713d1fb466abfeca3cb
tttt <dimongamer457@gmail.com>
2024-10-27 21:13:35 +0300

    27.10.2024

--- 2lesson.py
+++ 2lesson.py
@@ -1,49 +1,40 @@
 class Data:
-    print("oop")
+    print("Dogs")
     count = 0
-    def __init__(self, name = "no found", giga = 100, age = 0):
+    def __init__(self, name = "dog", size = 10, age = 0, color = "white", species = "dog"):
         self.name = name
-        self.giga = giga
+        self.size = size
         self.age = age
+        self.color = color
+        self.species = species
         Data.count += 1
     def addYear(self):
         if self.age < 20:
             self.age += 1
-
-
-    def process(self):
-        print("-Working-")
     def info(self):
         print(f"Name - {self.name}")
-        print(f"Giga - {self.giga}")
+        print(f"Size - {self.size}")
         print(f"Age - {self.age}")
+        print(f"Color - {self.color}")
+        print(f"Species - {self.species}")
     def __str__(self):
-        return f"Name - {self.name}\n" + f"Giga - {self.giga}\n" + f"Age - {self.age}"
+        return f"Name - {self.name}\n" + f"Size - {self.size}\n" + f"Age - {self.age}\n" + f"Color - {self.color}\n" + f"Species - {self.species}"
 
 
-print(Data.count)
 print("------------")
-data1 = Data("server", 991)
-#print(data1.name)
-#print(data1.giga)
-data1.addYear()
-data1.addYear()
-data1.addYear()
-data1.giga = 1080
-#print(data1.giga)
-#print(data1.age)
-data1.info()
-print(data1.count)
-data1.process()
+dog1 = Data("Dog1", 20, age = 1, color = "brown", species = "Bulldog")
+dog1.addYear()
+dog1.addYear()
+dog1.addYear()
+dog1.info()
+print("Count -", dog1.count)
 print("------------")
-data2 = Data("server2", 554)
-data2.info()
-print(data1.count)
-#print(data2.name)
-#print(data2.giga)
+dog2 = Data("Dog2", 30, age = 5, color = "black", species = "Shepherd")
+dog2.addYear()
+dog2.info()
+print("Count -", dog2.count)
 print("------------")
-data3 = Data()
-print(data3)
-print(data1.count)
-#print(data3.name)
-#print(data3.giga)
+dog3 = Data()
+dog3.info()
+print("Count -", dog3.count)
+print("------------")
