14e5f2672096d75cad5ec65d81810bdcf4c687c7
tttt <dimongamer457@gmail.com>
2024-11-02 15:41:37 +0200

    02.11.2024

--- simson 500.py
+++ simson 500.py
@@ -1,6 +1,5 @@
 from pyexpat import native_encoding
 
-
 class Human:
     def __init__(self, name, car = None, job = None):
         self.name = name
@@ -8,6 +7,7 @@ class Human:
         self.car = car
         self.job = job
         self.wallet = 50 # $
+        self.health = 100 # %
     def work(self):
         pass
     def sleep(self):
@@ -23,29 +23,39 @@ class Human:
     def live(self):
         pass
     def is_live(self):
-        if self.wallet < 0:
+        if self.wallet < 0 or self.health < 0:
             return False
         else:
             return True
+
 
 
 class House:
     def __init__(self):
         self.food = 0
         self.trash = 0
+    def clean(self):
+        pass
+    def fridge(self):
+        pass
 
 class Car:
     def __init__(self, model):
         self.model = model
         self.fuel = 60 # L
         self.state = 100 # %
-
+    def drive(self):
+        pass
+    def crash(self):
+        pass
 
 
 class Job:
     def __init__(self, name, cash):
         self.name = name
         self.cash = cash
+    def taking(self):
+        pass
 #car = Car("BMW")
 job = Job("Programmer", 1000)
 human = Human("Ivan", job = Job)
