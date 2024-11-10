37d6c6f2da24aafcf84d486653feef85c7519c94
tttt <dimongamer457@gmail.com>
2024-11-10 16:34:53 +0200

    10.11.2024

--- servers data.py
+++ servers data.py
@@ -1,12 +1,25 @@
 class Block():
-    def __init__(self, name):
+    def __init__(self, name = "Server"):
         self.name = name
+        self.giga = 1000
+        self.data = 896
+        self.age = 1
+    def info(self):
+        print("="*30)
+        print(f"Name - {self.name}")
+        print(f"Giga - {self.giga}")
+        print(f"Data - {self.data}")
+        print(f"Age  - {self.age}")
+        print("=" * 30)
 
+b = Block()
 class Server1(Block):
-    pass
-
+    b.info()
 class Server2(Block):
-    pass
-
+    b.info()
 class Server3(Block):
-    pass
+    b.info()
+
+s1 = Server1()
+s2 = Server2()
+s3 = Server3()
