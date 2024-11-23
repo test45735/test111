3aed96f64423ccd86edb054a68c15afa09718085
tttt <dimongamer457@gmail.com>
2024-11-23 19:23:55 +0200

    19.11.2024

--- lessson 9.py
+++ lessson 9.py
@@ -5,19 +5,32 @@ cur = connection.cursor()
 # print(connection)
 # print(cur)
 
+# == DROP ==
+cur.execute("DROP TABLE Animal;")
+connection.commit()
+
 # === CREATE ===
-# cur.execute("CREATE TABLE users (login TEXT);")
-# connection.commit()
+cur.execute("CREATE TABLE IF NOT EXISTS Animal (id INT, name TEXT, type TEXT);")
+connection.commit()
 
 # ==== INSERT ====
-name = input("Login : ")
-cur.execute(f"INSERT INTO users (login) VALUES ('{name}');")
-connection.commit()
-print("Login added!")
+# name = input("Login : ")
+# cur.execute(f"INSERT INTO users (login) VALUES ('{name}');")
+# connection.commit()
+# print("Login added!")
 
 # ===== SELECT =====
-cur.execute(f"SELECT rowid, login FROM users;")
-res = cur.fetchall()
-print(res)
+# cur.execute(f"SELECT rowid, login FROM users;")
+# res = cur.fetchall()
+# print(res)
+
+# ====== UPDATE ======
+cur.execute(f"INSERT INTO Animal (id, name, type) VALUES (1, 'Tiger', 'Walking');")
+cur.execute(f"INSERT INTO Animal (id, name, type) VALUES (2, 'Bear', 'Walking');")
+cur.execute(f"INSERT INTO Animal (id, name, type) VALUES (3, 'Dog', 'Walking');")
+cur.execute(f"INSERT INTO Animal (id, name, type) VALUES (4, 'Eagle', 'Flying');")
+cur.execute(f"INSERT INTO Animal (id, name, type) VALUES (5, 'Crow', 'Flying');")
+cur.execute(f"INSERT INTO Animal (id, name, type) VALUES (6, 'Salmon', 'Swimming');")
+connection.commit()
 
 connection.close()
